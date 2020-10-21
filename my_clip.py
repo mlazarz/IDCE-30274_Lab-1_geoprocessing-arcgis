d207cc5d0565b88d23221f08b6f7506c53fddd89
mlazarz <70230584+mlazarz@users.noreply.github.com>
2020-10-21 14:01:13 -0400

    Update my_clip.py

--- my_clip.py
+++ my_clip.py
@@ -1,5 +1,15 @@
-# Name: YOUR NAME HERE 
-# Python Version: ENTER ASSIGNMENT TITLE HERE.
-# Description: ENTER A DESCRIIPTION ABOUT WHAT YOUR CODE IS SUPPOSED TO DO HERE: DESCRIBE INPUTS / OUTPUTS.
+# Author: Mitchell Lazarz
+# Creation Date: 16 October 2020
+# Python 2.7: my_clip.py
+# Description: This code uses the clip tool in ArcGIS with a lake shapefile as the input feature, a basin shapefile
+# as the clip feature, resulting in an output of all lakes within the basin shapefile.  The output lakes_myClip is
+# saved to the local drive.
 
-Leave the lines of comment code, above, but add your personal details. Then replace these two lines of code with your new code! 
+# Imports the arcpy site package
+import arcpy
+
+# Workspace environment folder is set
+arcpy.env.workspace = "C:/CLARK/PythonProgramming/CompProg_Lab1"
+
+# Clip tool is called with lakes and basin shapefiles as inputs, producing shapefile of all lakes within basin
+arcpy.Clip_analysis("Data_Lab_1_Geoprocessing_ArcGIS/lakes.shp","Data_Lab_1_Geoprocessing_ArcGIS/basin.shp","Results/lakes_myClip.shp")
